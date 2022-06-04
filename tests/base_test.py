bfc5256b04cd9ceca55f42944c3c38b517f7b8d7
Karol Kolanski <gralu86@gmail.com>
2022-06-04 14:33:04 +0200

    headless added

--- tests/base_test.py
+++ tests/base_test.py
@@ -2,6 +2,7 @@ from pages.home_page import HomePage
 from tests.test_data import TestData
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import unittest
 
 class BaseTest(unittest.TestCase):
@@ -9,8 +10,9 @@ class BaseTest(unittest.TestCase):
     Base class for each test
     """
     def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
         self.driver.get("http://automationpractice.com/")
         self.driver.implicitly_wait(10)
         # Stworzyć instancję klasy HomePage
